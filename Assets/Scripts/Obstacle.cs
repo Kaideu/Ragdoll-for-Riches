beb9c0e5bfe7e55e686b87ea5c8c96f9acb7b2f1
Kaideu <44305564+Kaideu@users.noreply.github.com>
2024-03-09 15:37:40 -0600

    Updated obstacles + limb hit conditions

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -4,9 +4,29 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void Update()
     {
-        Destroy(gameObject, 10);
+        if (!IsVisibleOnScreen(gameObject))
+        {
+            Destroy(gameObject, 2);
+        }
+    }
+
+    private bool IsVisibleOnScreen(GameObject target)
+    {
+        Camera mainCam = Camera.main;
+        Vector3 targetScreenPoint = mainCam.WorldToScreenPoint(target.GetComponent<Renderer>().bounds.center);
+
+        if ((targetScreenPoint.x < 0) || (targetScreenPoint.x > Screen.width) ||
+                (targetScreenPoint.y < 0) || (targetScreenPoint.y > Screen.height))
+        {
+            return false;
+        }
+
+        if (targetScreenPoint.z < 0)
+        {
+            return false;
+        }
+        return true;
     }
 }
--- Assets/Scripts/Player/AddMoney.cs
+++ Assets/Scripts/Player/AddMoney.cs
@@ -6,11 +6,27 @@ public class AddMoney : MonoBehaviour
 {
     [SerializeField]
     LayerMask obstacles;
+
+    GameObject _lastHit;
+    Rigidbody _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
     public enum BodyPart{head, torso, arm, thigh, forearm, shin}
     public BodyPart bodyPart;
     private void OnCollisionExit(Collision other) {
-        if (Kaideu.Utils.Helpers.IsInLayerMask(obstacles, other.gameObject.layer)){
-            MoneyManager.Instance.UpdateCollectedBalance(bodyPart);
+
+        if (other.gameObject != _lastHit && Kaideu.Utils.Helpers.IsInLayerMask(obstacles, other.gameObject.layer))
+        {
+            if (_rb.velocity.y > 10)
+                MoneyManager.Instance.UpdateCollectedBalance(bodyPart);
+            else
+                Debug.Log($"Speed not enough for money: {bodyPart}");
+
+            _lastHit = other.gameObject;
         }
     }
 }
