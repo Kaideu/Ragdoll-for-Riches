a428f7d411e2bd8fe6b83ec135904a8cd1c53a65
Kaideu <44305564+Kaideu@users.noreply.github.com>
2024-03-10 00:20:30 -0600

    Update PhysicsManager.cs

--- Assets/Scripts/Physics/PhysicsManager.cs
+++ Assets/Scripts/Physics/PhysicsManager.cs
@@ -13,15 +13,17 @@ namespace Kaideu.Physics
         [SerializeField]
         float _terminalVelocity = 50;
         public float TVel => _terminalVelocity;
-        float _oTV;
+
+        float _maxTV;
+        public float MaxTV => _maxTV;
 
         private void Start()
         {
-            _oTV = _terminalVelocity;
+            _maxTV = _terminalVelocity;
         }
 
         public void SetTerminalVelocity(float amount) => _terminalVelocity = amount;
 
-        public void ResetTerminalVelocity() => _terminalVelocity = _oTV;
+        public void ResetTerminalVelocity() => _terminalVelocity = _maxTV;
     }
 }
